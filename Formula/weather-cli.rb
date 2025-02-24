592fc73864c32957abc843d0c4865d51113f5a9d
GloryWong <glorywong1001@gmail.com>
2025-02-24 17:32:27 +0000

    Update weather-cli formula for release

--- Formula/weather-cli.rb
+++ Formula/weather-cli.rb
@@ -1,23 +1,23 @@
 class WeatherCli < Formula
   desc "A CLI tool for weather information"
   homepage "https://github.com/GloryWong/weather-cli"
-  version "v0.0.0" # This will be updated automatically
+  version "v1.0.14" # This will be updated automatically
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/GloryWong/weather-cli/releases/download/#{version}/weather-cli.aarch64-apple-darwin.tar.gz"
-      sha256 "SHA256_AARCH64_DARWIN_PLACEHOLDER"
+            url "https://github.com/GloryWong/weather-cli/releases/download/v1.0.14/weather-cli.aarch64-apple-darwin.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/GloryWong/weather-cli/releases/download/#{version}/weather-cli.x86_64-apple-darwin.tar.gz"
-      sha256 "SHA256_X86_64_DARWIN_PLACEHOLDER"
+            url "https://github.com/GloryWong/weather-cli/releases/download/v1.0.14/weather-cli.x86_64-apple-darwin.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/GloryWong/weather-cli/releases/download/#{version}/weather-cli.aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "SHA256_AARCH64_LINUX_PLACEHOLDER"
+            url "https://github.com/GloryWong/weather-cli/releases/download/v1.0.14/weather-cli.aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/GloryWong/weather-cli/releases/download/#{version}/weather-cli.x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "SHA256_X86_64_LINUX_PLACEHOLDER"
+            url "https://github.com/GloryWong/weather-cli/releases/download/v1.0.14/weather-cli.x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
