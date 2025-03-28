2fc77aae7884e60b3606a157260c0291d20b24b3
Glory Wong <glorywong1001@gmail.com>
2025-03-28 23:52:56 +0800

    Update updown.rb to run upload with notification for fatal error

--- Formula/updown.rb
+++ Formula/updown.rb
@@ -5,19 +5,19 @@ class Updown < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-                                                                                                                                                                                                url "https://github.com/GloryWong/updown/releases/download/v1.4.2/updown.aarch64-apple-darwin.tar.gz"
-                                                                                                                                                                        sha256 "2f6cf58575ccdfb22b2b956088ccb705b8478f71b4e87a404bc3fa9d1b8906db"
+      url "https://github.com/GloryWong/updown/releases/download/v1.4.2/updown.aarch64-apple-darwin.tar.gz"
+      sha256 "2f6cf58575ccdfb22b2b956088ccb705b8478f71b4e87a404bc3fa9d1b8906db"
     else
-                                                                                                                                                                                                url "https://github.com/GloryWong/updown/releases/download/v1.4.2/updown.x86_64-apple-darwin.tar.gz"
-                                                                                                                                                                        sha256 "f00263822e30808f15839db84a3336aac43394b3c397e6626c28e33f525cc927"
+      url "https://github.com/GloryWong/updown/releases/download/v1.4.2/updown.x86_64-apple-darwin.tar.gz"
+      sha256 "f00263822e30808f15839db84a3336aac43394b3c397e6626c28e33f525cc927"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-                                                                                                                                                                                                url "https://github.com/GloryWong/updown/releases/download/v1.4.2/updown.aarch64-unknown-linux-gnu.tar.gz"
-                                                                                                                                                                        sha256 "fb574fa8ce0e1a4876c6935eeeaea62912d4e92db6e1bcecbf748a6add15a7c6"
+      url "https://github.com/GloryWong/updown/releases/download/v1.4.2/updown.aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "fb574fa8ce0e1a4876c6935eeeaea62912d4e92db6e1bcecbf748a6add15a7c6"
     else
-                                                                                                                                                                                                url "https://github.com/GloryWong/updown/releases/download/v1.4.2/updown.x86_64-unknown-linux-gnu.tar.gz"
-                                                                                                                                                                        sha256 "f689b851cab335e73397282fbe75494760dfe443fb9607dd386e6fdd431ba224"
+      url "https://github.com/GloryWong/updown/releases/download/v1.4.2/updown.x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "f689b851cab335e73397282fbe75494760dfe443fb9607dd386e6fdd431ba224"
     end
   end
 
@@ -44,7 +44,7 @@ class Updown < Formula
   end
 
   service do
-    run [opt_bin/"updown", "upload", "--quiet"]
+    run [opt_bin/"updown", "upload", "--quiet", "--notify"]
     run_type :cron
     cron "0 15 * * *" # 15:00 every day
     log_path Pathname.new(Dir.home)/".updown/logs/updown-brew-service.log"
